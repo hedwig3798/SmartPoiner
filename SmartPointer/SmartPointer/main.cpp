19825a16005c0782d51de00a768ea76f71cf8550
Hyenghwan Kim <joey3798@naver.com>
2025-04-30 16:55:00 +0900

    커스텀 할당자와 연결 성공
    테스트 결과 일단 연결 성공
    예외 사항 생각 날 때마다 수정할 예정

--- SmartPointer/SmartPointer/main.cpp
+++ SmartPointer/SmartPointer/main.cpp
@@ -4,9 +4,24 @@
 #include "WeakPointer.h"
 #include <iostream>
 
+#define MEM_SIZE 1'048'576'000 //1GB
+
 int main()
 {
-	SharedPointer<int> sharedTestA = SharedPointer<int>(1);
+#ifdef _DEBUG
+	std::string path("./CustomAllocatorD.dll");
+#else
+	std::string path("./CustomAllocator.dll");
+#endif
+	HMODULE hDLL = ::LoadLibraryA(path.c_str());
+	if (hDLL == nullptr)
+	{
+		std::cout << "Cannot Link DLL\n";
+		return 0;
+	}
+	IAllocator* listAllocator = ((IAllocator * (*)(size_t, unsigned char, size_t))GetProcAddress(hDLL, "CreateListAllocator"))(MEM_SIZE, 0, 16);
+
+	SharedPointer<int> sharedTestA = SharedPointer<int>(1, listAllocator, listAllocator);
 	SharedPointer<int> sharedTestB = sharedTestA;
 	WeakPointer<int> weakTestC = sharedTestA;
 
@@ -21,7 +36,7 @@ int main()
 
 	std::cout << *sharedTestA << " " << *sharedTestB << " " << *(weakTestC.Lock()) << "\n";
 
-	UniquePointer<int> uniqueTestA = UniquePointer<int>(1);
+	UniquePointer<int> uniqueTestA = UniquePointer<int>(1, listAllocator);
 	std::cout << *uniqueTestA << "\n";
 	UniquePointer<int> uniqueTestB = std::move(uniqueTestA);
 	// uniqueTestB = uniqueTestA;
